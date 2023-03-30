3565c1bac6a28dc60b12187ad4317d0f5075ae10
maskon <126991331+maskon@users.noreply.github.com>
2023-03-30 21:23:12 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,83 @@
+/* Поэкранный скролл */
+
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        menu: '#menu',
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+        navigation: {
+            'textColor': '#f2f2f2',
+            'bulletsColor': '#ccc',
+            'position': 'right',
+            'tooltips': ['Page 1', 'Page 2', 'Page 3', 'Page 4', 'Page 5', 'Page 6']
+        }
+    });
+});
+
+
+    /* Nav Toggle */
+
+     $("#burger").on("click", function(event) {
+        event.preventDefault();
+
+        $("#menu").toggleClass("show");
+        $("body").toggleClass("no-scroll");
+    });
+
+
+/* Modal на HTML CSS и jQuery */
+
+    const modalCall = $("[data-modal]");
+    const modalClose = $("[data-close]");
+
+    modalCall.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalId = $this.data('modal');
+
+        $(modalId).addClass('show');
+        $("body").addClass('no-scroll');
+
+        setTimeout(function() {
+            $(modalId).find(".modal__dialog").css({
+                transform: "scale(1)"
+            });
+        }, 200);
+    });
+
+
+    modalClose.on("click", function(event) {
+        event.preventDefault();
+
+        let $this = $(this);
+        let modalParent = $this.parents('.modal');
+
+
+        modalParent.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            modalParent.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+
+    $(".modal").on("click", function(event) {
+        let $this = $(this);
+
+        $this.find(".modal__dialog").css({
+            transform: "scale(0)"
+        });
+
+        setTimeout(function() {
+            $this.removeClass('show');
+            $("body").removeClass('no-scroll');
+        }, 200);
+    });
+
+    $(".modal__dialog").on("click", function(event) {
+        event.stopPropagation();
+    });
+
